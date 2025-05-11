daf13c46fabd53db7b939cd9b5b80cc3f867ba3c
ta1355 <eorbs255@naver.com>
2025-05-11 22:08:58 +0900

    first commit

---
+++ src/main/java/com/yoo/hiddenpixels/HiddenpixelsApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.hiddenpixels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddenpixelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiddenpixelsApplication.class, args);
+	}
+
+}
